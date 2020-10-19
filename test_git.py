04d2cc8786a2fa3c5101c3a6e12a7c8794d0b2a8
Marie Dausse <marie.daussee@gmail.com>
2020-10-19 06:21:26 +0200

    shortest version

--- test_git.py
+++ test_git.py
@@ -5,5 +5,5 @@ continuer = 'o'
 
 while continuer == 'o':
     print("Le compteur est maintenant à {}".format(n))
-    print("Voulez-vous continuer ? o/n")
+    continuer = input("Voulez-vous continuer ? o/n")
     n += 1
